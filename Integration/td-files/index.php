bf29cd6b2130c29f292397e38c0e70b5a246242d
Soufian AIT TIRITE <aittirite.soufian@gmail.com>
2017-11-08 16:39:43 +0100

    integration TD update

--- Integration/td-files/index.php
+++ Integration/td-files/index.php
@@ -6,6 +6,261 @@
 	<title>Titre de la page</title>
 </head>
 <body>
-	
+	<header>
+		<section>
+			<nav>
+				<ul>
+					<li><a href="#">Resume</a></li>
+				</ul>
+			</nav>
+		</section>
+		<section>
+			<div class="name">
+				<h1>Edouard Sombié</h1>
+				<h2>Développeur Web</h2>
+			</div>
+			<figure class="photo">
+				<img src="#" alt="Texte alternatif" title="Titre de mon image">
+			</figure>
+		</section>
+	</header>
+	<main>
+		<section>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+		</section>
+		<section>
+			<section class="left">
+				<article>
+					<h1>Personnal</h1>
+					<table>
+						<tr>
+							<td>Name</td>
+							<td>Edouard Sombié</td>
+						</tr>
+						<tr>
+							<td>Birthday</td>
+							<td>Edouard Sombié</td>
+						</tr>
+						<tr>
+							<td>Relationship</td>
+							<td>Edouard Sombié</td>
+						</tr>
+						<tr>
+							<td>Nationality</td>
+							<td>Edouard Sombié</td>
+						</tr>
+						<tr>
+							<td>Languages</td>
+							<td>Edouard Sombié</td>
+						</tr>
+						<tr>
+							<td>Telephone</td>
+							<td>Edouard Sombié</td>
+						</tr>
+					</table>
+				</article>
+				<article>
+					<h1>Software</h1>
+					<table>
+						<tr>
+							<td>Indesign CC</td>
+							<td><progress value="1" max="1"></td>
+						</tr>
+						<tr>
+							<td>Illustrator</td>
+							<td><progress value="0.9" max="1"></td>
+						</tr>
+						<tr>
+							<td>Photoshop CC</td>
+							<td><progress value="0.8" max="1"></td>
+						</tr>
+						<tr>
+							<td>Sketch 3</td>
+							<td><progress value="0.7" max="1"></td>
+						</tr>
+						<tr>
+							<td>HTML 5</td>
+							<td><progress value="0.6" max="1"></td>
+						</tr>
+						<tr>
+							<td>CSS 3</td>
+							<td><progress value="0.5" max="1"></td>
+						</tr>
+					</table>
+				</article>
+				<article>
+					<h1>Hobbies</h1>
+					<ul>
+						<li>
+							<figure>
+								<img src="">
+								<figcaption>Outdoors</figcaption>
+							</figure>
+						</li>
+						<li>
+							<figure>
+								<img src="">
+								<figcaption>Travelling</figcaption>
+							</figure>
+						</li>
+						<li>
+							<figure>
+								<img src="">
+								<figcaption>Basketball</figcaption>
+							</figure>
+						</li>
+					</ul>
+					<ul>
+						<li>
+							<figure>
+								<img src="">
+								<figcaption>Writing</figcaption>
+							</figure>
+						</li>
+						<li>
+							<figure>
+								<img src="">
+								<figcaption>Socializing</figcaption>
+							</figure>
+						</li>
+						<li>
+							<figure>
+								<img src="">
+								<figcaption>Gaming</figcaption>
+							</figure>
+						</li>
+					</ul>
+				</article>
+			</section>
+			<section class="right">
+				<article>
+					<h1>Work</h1>
+					<ul>
+						<li>
+							<h3>Design Director</h3>
+							<time>2015</time>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua.
+							</p>
+						</li>
+						<li>
+							<h3>Senior Designer</h3>
+							<time>2012</time>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua.
+							</p>
+						</li>
+						<li>
+							<h3>Designer</h3>
+							<time>2008</time>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua.
+							</p>
+						</li>
+					</ul>
+				</article>
+				<article>
+					<h1>Professionnal Skills</h1>
+					<ul>
+						<li>
+							<figure>
+								<img src="">
+								<figcaption>Design</figcaption>
+							</figure>
+						</li>
+						<li>
+							<figure>
+								<img src="">
+								<figcaption>Concepts</figcaption>
+							</figure>
+						</li>
+						<li>
+							<figure>
+								<img src="">
+								<figcaption>Art Direction</figcaption>
+							</figure>
+						</li>
+					</ul>
+					<ul>
+						<li>
+							<figure>
+								<img src="">
+								<figcaption>Teamwork</figcaption>
+							</figure>
+						</li>
+						<li>
+							<figure>
+								<img src="">
+								<figcaption>Management</figcaption>
+							</figure>
+						</li>
+					</ul>
+				</article>
+				<article>
+					<h1>Education</h1>
+					<ul>
+						<li>
+							<h3>MA Graphics</h3>
+							<time>2006</time>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua.
+							</p>
+						</li>
+						<li>
+							<h3>BA(hons) Graphics</h3>
+							<time>2003</time>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua.
+							</p>
+						</li>
+					</ul>
+				</article>
+			</section>
+		</section>
+	</main>
+	<footer>
+		<section>
+			<a href="#" title="Follow-my">
+				<figure>
+					<img src="#">
+					<figcaption>www.behance.com/davidbrown</figcaption>
+				</figure>
+			</a>
+			<a href="#" title="Follow-my">
+				<figure>
+					<img src="#">
+					<figcaption>www.linkedin.com/davidbrown</figcaption>
+				</figure>
+			</a>
+			<a href="#" title="Follow-my">
+				<figure>
+					<img src="#">
+					<figcaption>www.twitter.com/davidbrown</figcaption>
+				</figure>
+			</a>
+		</section>
+		<section>
+			<p>
+				Flat 2, 10 Broadway, New York, NY 12345. Call: +1 (212) 123-4567. Email: davidbrown@gmail.com
+			</p>
+			<p>
+				<a href="#" title="Visit my website">
+					www.davidbrown.com
+				</a>
+			</p>
+		</section>
+	</footer>
 </body>
 </html>
