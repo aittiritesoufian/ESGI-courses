3f28f92b65a0e7241140ce739178847ca7b3de4d
Soufian AIT TIRITE <aittirite.soufian@gmail.com>
2017-11-08 15:06:39 +0100

    cours integration

---
+++ Integration/td-files/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="La description de ma page.">
+	<title>Titre de la page</title>
+</head>
+<body>
+	
+</body>
+</html>
