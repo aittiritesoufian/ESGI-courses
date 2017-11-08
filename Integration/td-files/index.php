2c42675301a9b840f3a62b13d9c1234ca418d056
Soufian AIT TIRITE <aittirite.soufian@gmail.com>
2017-11-08 18:47:30 +0100

    update TD & add fonts

--- Integration/td-files/index.php
+++ Integration/td-files/index.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="description" content="La description de ma page.">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<title>Titre de la page</title>
 </head>
 <body>
