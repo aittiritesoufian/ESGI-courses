de7b1d195da1281b76df34cc30969dce316e1ce2
Soufian AIT TIRITE <aittirite.soufian@gmail.com>
2018-01-07 20:01:19 +0100

    add td

--- Java/JAVAworkspace/19_12_2017/td3/src/Ex2_2/Main.java
+++ Java/JAVAworkspace/19_12_2017/td3/src/Ex2_2/Main.java
@@ -7,30 +7,29 @@ import java.util.Comparator;
 public class Main {
 	public static void main(String[] args) {
 		Etudiant e1 = new Etudiant("Dupont1", 21);
-		Etudiant e2 = new Etudiant("Bloom", 36);
+		Etudiant e2 = new Etudiant("BloomA", 36);
+		Etudiant e2bis = new Etudiant("BloomB", 22);
+		Etudiant e2ter = new Etudiant("BloomC", 36);
 		Etudiant e3 = new Etudiant("Dupont2", 15);
 		
 		ArrayList<Etudiant> list = new ArrayList<>();
 		
 		list.add(e1);
+		list.add(e2ter);
 		list.add(e2);
+		list.add(e2bis);
 		list.add(e3);
 
 		System.out.println(list);
-		Collections.sort(list, new Etudiant());
+		Collections.sort(list, new EtudiantComparator());
 		System.out.println(list);
 	}
 }
 
-class Etudiant implements Comparator<Etudiant>{
+class Etudiant {
 	String nom;
 	int age;
 	ArrayList<Etudiant> list;
-
-	public Etudiant() {
-		this.nom = "AIT TIRITE";
-		this.age = 3;
-	}
 	
 	public Etudiant(String nom, int age) {
 		this.nom = nom;
@@ -55,26 +54,19 @@ class Etudiant implements Comparator<Etudiant>{
 	public String toString() {
 		return "nom : "+this.nom+" age : "+this.age;
 	}
+}
 
-//	@Override
-//	public int compareTo(Etudiant arg0) {
-////		if(arg0.age<this.age){
-////			return 1;
-////		}
-//		if(arg0.nom.compareToIgnoreCase(this.nom)<0){
-//			return 1;
-//		}
-//		return -1;
-//	}
+class EtudiantComparator implements Comparator<Etudiant> {
 
 	@Override
 	public int compare(Etudiant arg0, Etudiant arg1) {
-		if(arg0.age<this.age){
+		if(arg0.age>arg1.age){
 			return 1;
 		}
-		if(arg0.nom.compareToIgnoreCase(this.nom)<0){
+		if(arg0.nom.compareToIgnoreCase(arg1.nom)<0){
 			return -1;
 		}
-		return 0;
+		return -1;
 	}
+	
 }
